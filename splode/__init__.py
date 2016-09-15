98a5f89d9b85691a6ba11dfa2c7145de964f873b
Sybren A. Stüvel <sybren@stuvel.eu>
2016-09-15 09:58:00 +0200

    Added 'find cycles' operator + button

--- splode/__init__.py
+++ splode/__init__.py
@@ -35,19 +35,49 @@ class FILE_OT_splode(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class SPLODE_OT_find_cycles(bpy.types.Operator):
+    bl_idname = 'splode.find_cycles'
+    bl_label = 'Find cycles'
+    bl_options = {'REGISTER'}
+
+    def execute(self, context):
+        import itertools
+        import logging
+        from . import internal, depcycles
+
+        log = logging.getLogger('%s.find_cycles' % __name__)
+        user_map = internal.selective_user_map()
+
+        cycle_gen = depcycles.find_cycles(user_map)
+        cycles = depcycles.unify_cycles(cycle_gen)
+
+        if cycles:
+            log.info('Found %i disjoint cycles:', len(cycles))
+            for cycle in itertools.islice(cycles, 10):
+                log.info('   - %s', cycle)
+            depcycles.assert_disjoint(cycles)
+        else:
+            log.info('Found no cycles')
+
+        return {'FINISHED'}
+
+
 def draw_info_header(self, context):
     layout = self.layout
     layout.operator(FILE_OT_splode.bl_idname, text='Splode')
+    layout.operator(SPLODE_OT_find_cycles.bl_idname)
     layout.operator('object.make_local', text='Make all local').type = 'ALL'
 
 
 def register():
     bpy.utils.register_class(FILE_OT_splode)
+    bpy.utils.register_class(SPLODE_OT_find_cycles)
     bpy.types.VIEW3D_HT_header.append(draw_info_header)
 
 
 def unregister():
     bpy.utils.unregister_class(FILE_OT_splode)
+    bpy.utils.unregister_class(SPLODE_OT_find_cycles)
     bpy.types.VIEW3D_HT_header.remove(draw_info_header)
 
 
